de18646aaa85f6ce5b9d474a789b7885e36de72b
Sean Yesmunt <sean@lbry.io>
2020-07-30 10:05:42 -0400

    drop-shadow

--- ui/component/header/view.jsx
+++ ui/component/header/view.jsx
@@ -221,7 +221,7 @@ const Header = (props: Props) => {
                 }}
                 {...homeButtonNavigationProps}
               >
-                <img src={OdyseeLogo} />
+                <img src={OdyseeLogo} className="header__odysee" />
               </Button>
 
               {/* <Button
